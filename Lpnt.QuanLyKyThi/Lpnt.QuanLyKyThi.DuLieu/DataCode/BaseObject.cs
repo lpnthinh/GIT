9e2962ae40579f3a4008bf356ae4cfbd3755b5f8
lephucnhathinh <lpnt.it@gmail.com>
2017-01-19 03:05:36 +0700

    Tao Lpnt.QuanLyKyThi

---
+++ Lpnt.QuanLyKyThi/Lpnt.QuanLyKyThi.DuLieu/DataCode/BaseObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace Lpnt.QuanLyKyThi.DuLieu
+{
+
+    public partial class BaseObject
+    {
+        public BaseObject(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ Lpnt.QuanLyKyThi/Lpnt.QuanLyKyThi/FrmTrangChu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lpnt.QuanLyKyThi
+{
+    public partial class FrmTrangChu : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FrmTrangChu()
+        {
+            InitializeComponent();
+        }
+    }
+}
